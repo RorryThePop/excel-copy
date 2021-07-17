c4eef728d037981f1258be8fc09c1ef02285b68a
Arakelove Yury <72825634+RorryThePop@users.noreply.github.com>
2021-07-17 16:33:24 +0300

    start of framework

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import './module';
 import './styles/index.scss';
 
 console.log('Working!');
-
-const val = document.getElementById('root');
-const text = document.createElement('p');
-const textValue = document.createTextNode('assalam');
-const a = val.appendChild(text);
-a.appendChild(textValue);
